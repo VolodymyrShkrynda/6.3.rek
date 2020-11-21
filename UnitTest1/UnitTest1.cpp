1a9747639b87cbc878dedb46289bcc48051c5258
VolodymyrShcrynda <volodymyr.shkrynda.itis.2020@lpnu.ua>
2020-11-21 23:01:39 +0200

    first commit

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,22 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../6.3.2/Source.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int t;
+			const int n = 4;
+			int a[n] = { 1,2,3,4 };
+			t = Sum(a, n,0,0);
+
+		}
+	};
+}
